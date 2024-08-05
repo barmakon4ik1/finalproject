113c3a10c8be9564e097210a7dab2c7c5e5576f6
barmakon4ik <barmakon4ik@gmail.com>
2024-08-05 08:08:27 +0200

    "Adress.object added, def_title2+3 add"

--- housing/models.py
+++ housing/models.py
@@ -2,17 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Address(models.Model):
-    country = models.CharField(_('Country'), max_length=100)
-    city = models.CharField(_('City'), max_length=100)
-    street = models.CharField(_('Street'), max_length=100)
-    house_number = models.IntegerField(_('Haus'), )
-    postal_code = models.CharField(_('Post'), max_length=100)
-
-    def __str__(self):
-        return f'{self.street}, {self.house_number}, {self.postal_code} {self.city}, {self.country}'
-
-
 class Housing(models.Model):
     HOUSING_CHOICES = [
         ('H', _('Haus')),
@@ -27,9 +16,19 @@ class Housing(models.Model):
     rooms = models.IntegerField(_('Rooms'), )
     type = models.CharField(_('Type'), max_length=50, choices=HOUSING_CHOICES)
 
-
     def __str__(self):
         return self.name
 
+
+class Address(models.Model):
+    country = models.CharField(_('Country'), max_length=25)
+    city = models.CharField(_('City'), max_length=20)
+    street = models.CharField(_('Street'), max_length=100)
+    house_number = models.CharField(_('Haus'), max_length=5)
+    postal_code = models.IntegerField(_('Post'), max_length=8)
+    object = models.ForeignKey(Housing, on_delete=models.CASCADE, related_name='addresses', verbose_name=_('Object'), blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.street}, {self.house_number}, {self.postal_code} {self.city}, {self.country}'
 
 
