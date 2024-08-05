846ff58883c43b842a0b7b540271246b25a0e83d
Barmakon4ik <barmakon4ik1@gmail.com>
2024-08-05 18:59:20 +0200

    "Address+Housing linked"

--- housing/admin.py
+++ housing/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from housing.models import *
 
+
 admin.site.register(Address)
+
 admin.site.register(Housing)
--- housing/models.py
+++ housing/models.py
@@ -6,11 +6,14 @@ class Address(models.Model):
     country = models.CharField(_('Country'), max_length=100)
     city = models.CharField(_('City'), max_length=100)
     street = models.CharField(_('Street'), max_length=100)
-    house_number = models.IntegerField(_('Haus'), )
+    house_number = models.CharField(_('Haus'), max_length=6)
     postal_code = models.CharField(_('Post'), max_length=100)
 
     def __str__(self):
         return f'{self.street}, {self.house_number}, {self.postal_code} {self.city}, {self.country}'
+
+    class Meta:
+        verbose_name = _('Addresses')
 
 
 class Housing(models.Model):
@@ -26,7 +29,9 @@ class Housing(models.Model):
     price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2)
     rooms = models.IntegerField(_('Rooms'), )
     type = models.CharField(_('Type'), max_length=50, choices=HOUSING_CHOICES)
-
+    address = models.OneToOneField(Address, on_delete=models.SET_NULL, null=True, related_name='housing')
+    deleted = models.BooleanField(_('Deleted'), default=False)
+    deleted_at = models.DateTimeField(_('Deleted at'), null=True, blank=True)
 
     def __str__(self):
         return self.name
